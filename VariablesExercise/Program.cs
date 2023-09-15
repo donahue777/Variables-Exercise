ad4e59f1d3be647d6e6e4a2e947497a6c7eb60a0
donahue777 <donahue777@yahoo.com>
2023-09-15 07:49:30 -0400

    taco1

--- VariablesExercise/Program.cs
+++ VariablesExercise/Program.cs
@@ -4,7 +4,18 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string foodName = "Taco";
+            int foodAge = 4;
+            char firstLetter = 'T';
+            bool isTasty = true;
+            double foodWeight = 1.2;
+            decimal foodPrice = 2.75m;
+            Console.WriteLine($"Hi. I am eating a {foodName} and it is {foodAge} months old.");
+            Console.WriteLine($"My {foodName}'s first letter starts with a {firstLetter}.");
+            Console.WriteLine($"It is {isTasty} how scrumptious this {foodName} tastes right now.");
+            Console.WriteLine($"The {foodName} weighs {foodWeight} pounds and only costed me {foodPrice} {foodAge} months ago.");
+
+            Console.WriteLine(":D");
         }
     }
 }
